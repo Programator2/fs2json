163b501ae1a82108954fca6cea0495bbe8108be0
Roderik Ploszek <roderik.ploszek@gmail.com>
2023-03-04 08:38:03 +0100

    feat: implement sqlite support

---
+++ db.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+import sqlite3
+
+
+class Database:
+    def __init__(self, path):
+        self.con = sqlite3.connect(path, isolation_level=None)
+        self.cur = self.con.cursor()
+        self.create_db()
+        self.cur.execute('PRAGMA synchronous = OFF')
+        self.cur.execute('PRAGMA journal_mode = MEMORY')
+        self.cur.execute('BEGIN TRANSACTION')
+
+    def create_db(self):
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS fs(parent INTEGER, name TEXT, ino INTEGER, dev INTEGER, nlink INTEGER, uid INTEGER, gid INTEGER, size INTEGER, atime INTEGER, mtime INTEGER, ctime INTEGER, type INTEGER, mode INTEGER)"
+        )
+
+    def insert_dentry(
+        self,
+        parent,
+        name,
+        ino,
+        dev,
+        nlink,
+        uid,
+        gid,
+        size,
+        atime,
+        mtime,
+        ctime,
+        _type,
+        mode,
+    ):
+        """Execute INSERT statement for a dentry.
+
+        :returns: rowid of inserted row
+        """
+        self.cur.execute(
+            'INSERT INTO fs VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            (
+                parent,
+                name,
+                ino,
+                dev,
+                nlink,
+                uid,
+                gid,
+                size,
+                atime,
+                mtime,
+                ctime,
+                _type,
+                mode,
+            ),
+        )
+        return self.cur.lastrowid
+
+    def close(self):
+        self.cur.execute('END TRANSACTION')
+        self.cur.execute('CREATE INDEX parent_index ON fs (parent)')
+        self.con.commit()
+        self.cur.close()
+        self.con.close()
+
+
+class DatabaseRead:
+    def __init__(self, path):
+        self.con = sqlite3.connect(path)
+        self.cur = self.con.cursor()
+
+    @staticmethod
+    def _create_path(path: str):
+        """Create necessary nodes in the tree to represent a path.
+
+        :param path: string in the form of `/this/is/a/path`. It has to start
+        with a `/` and optionally end with a `/`.
+        """
+        return filter(lambda x: bool(x), path.split('/'))
+
+    def search_path(self, path: str) -> tuple:
+        """Return dentry object from the database if it exists."""
+        entries = tuple(self._create_path(path))
+
+        # Root should be stored under rowid 1 in the database
+        current_folder = 1
+
+        for i, e in enumerate(entries):
+            if i == len(entries) - 1:
+                res = self.cur.execute(
+                    'SELECT * FROM fs WHERE parent = ? AND name = ?',
+                    (current_folder, e),
+                )
+                return res.fetchone()
+            res = self.cur.execute(
+                'SELECT rowid FROM fs WHERE parent = ? AND name = ?',
+                (current_folder, e),
+            )
+            current_folder = res.fetchone()[0]
+
+        return tuple()
+
+    def close(self):
+        self.cur.close()
+        self.con.close()
---
+++ fs2sql.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+from os import stat, listdir
+import os
+from stat import *
+import json
+import sys
+from db import Database
+
+
+def get_dentry(filename, path):
+    s = stat(path, follow_symlinks=False)
+    data = (
+        filename,
+        s.st_ino,
+        s.st_dev,
+        s.st_nlink,
+        s.st_uid,
+        s.st_gid,
+        s.st_size,
+        s.st_atime,
+        s.st_mtime,
+        s.st_ctime,
+        S_IFMT(s.st_mode),
+        S_IMODE(s.st_mode),
+    )
+    return (data, bool(S_ISDIR(s.st_mode)), bool(S_ISREG(s.st_mode)))
+
+
+def _walktree(top: str, parent: int):
+    '''Recursively descend the directory tree rooted at top, inserting dentries
+    into the database.
+    '''
+
+    for f in os.listdir(top):
+        pathname = os.path.join(top, f)
+        try:
+            data, isdir, isreg = get_dentry(f, pathname)
+        except (FileNotFoundError, PermissionError):
+            continue
+
+        # Insert into database
+        new_parent = db.insert_dentry(parent, *data)
+
+        if isdir:
+            _walktree(pathname, new_parent)
+
+
+def walktree(root: str, db: Database):
+    data, isdir, isreg = get_dentry(
+        os.path.basename(os.path.normpath(root)), root
+    )
+    parent = db.insert_dentry(None, *data)
+    if isdir:
+        _walktree(root, parent)
+
+
+db = Database('fs.db')
+walktree(sys.argv[1], db)
+db.close()
