f09f1c029712a8a009bfcede2ec84abf2a9b1265
Roderik Ploszek <roderik.ploszek@gmail.com>
2023-05-23 23:18:06 +0200

    feat: make database path customizable

--- fs2sql_cmd.py
+++ fs2sql_cmd.py
@@ -5,7 +5,9 @@ import sys
 
 
 if __name__ == '__main__':
-    db = DatabaseCreator('fs.db', drop=True)
+    if len(sys.argv) != 3:
+        print(f'Usage: {sys.argv[0]} dir output_path')
+    db = DatabaseCreator(sys.argv[2], drop=True)
     db.insert_unix_database()
     walktree(sys.argv[1], db)
     db.close()
